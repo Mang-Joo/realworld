715e4097e0d8e36bdcfeead900fd864852354008
JunnyJun <chbe5082@naver.com>
2023-01-10 13:07:30 +0900

    feat(jwt) : refactor provider

--- src/main/kotlin/realworld/mangjoo/auth/jwt/JwtCreateTokenUseCase.kt
+++ src/main/kotlin/realworld/mangjoo/auth/jwt/JwtCreateTokenUseCase.kt
@@ -9,29 +9,23 @@ import java.util.*
 fun interface JwtCreateTokenUseCase {
     fun createToken(email: String): String
 
-
     @Component
     class JwtCreateTokenProvider(
         @Value("\${jwt.token.secretKey}") val key: String,
         @Value("\${jwt.token.time}") val time: Long
     ) : JwtCreateTokenUseCase {
-        override fun createToken(email: String): String {
-            val claims = Jwts.claims()
-                .also { it.subject = email }
-                .also { it["password"] = email }
-
-            val header = Jwts.header()
-                .also { it["key"] = email }
-
-            val now = Date()
-
-            return Jwts.builder()
-                .setHeader(header)
-                .setClaims(claims)
+        private val now = Date()
+        override fun createToken(email: String): String = Jwts.builder()
+                .setHeader(header(email))
+                .setClaims(body(email))
                 .setIssuedAt(now)
                 .setExpiration(Date(now.time + time))
                 .signWith(SignatureAlgorithm.HS256, key.encodeToByteArray())
                 .compact()
-        }
+
+        private fun header(email: String) = Jwts.header().also { it["key"] = email }
+        private fun body(email: String) = Jwts.claims()
+            .also { it.subject = email }
+            .also { it["password"] = email }
     }
 }
